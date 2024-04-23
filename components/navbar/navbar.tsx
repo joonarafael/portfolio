0b3b5da2156d6c13691097a38f86e609d849eef1
joonarafael <joona.kettunen21@gmail.com>
2024-04-23 11:38:43 +0300

    hydration fails as usual

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,20 +1,24 @@
 "use client";
 
-import ContactButton from './contact';
-import Logo from './logo';
-import Shortcuts from './shortcuts';
-import ThemeSwitch from './themeswitch';
+import dynamic from "next/dynamic";
+
+import ContactButton from "./contact";
+import Shortcuts from "./shortcuts";
+import ThemeSwitch from "./themeswitch";
+
+const Logo = dynamic(() => import("./logo"), { ssr: false });
 
 const Navbar = () => {
 	return (
 		<div className="z-50 p-12 group fixed flex w-full justify-center items-center bg-gradient-to-b from-background to-transparent">
 			<div className="group-hover:shadow-lg group-hover:shadow-black/50 dark:group-hover:shadow-purple-500 dark:shadow-purple-800 shadow-black/25 bg-background border shadow-md min-w-[220px] w-full max-w-[2440px] flex justify-between items-center rounded-xl p-2 transition group-hover:duration-200 duration-1000 flex-col sm:flex-row gap-2">
 				<Logo />
-				{!window.location.href.includes("projects") && (
-					<div className="hidden lg:block">
-						<Shortcuts />
-					</div>
-				)}
+				{typeof window !== "undefined" &&
+					!window.location.href.includes("projects") && (
+						<div className="hidden lg:block">
+							<Shortcuts />
+						</div>
+					)}
 				<div className="flex flex-row gap-2">
 					<ThemeSwitch />
 					<ContactButton />
