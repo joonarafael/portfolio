adb682698f5845db24db1db92a7f3d5f5c1f92fb
joonarafael <joona.kettunen21@gmail.com>
2024-04-24 11:27:24 +0300

    remove aws

--- lib/learning.ts
+++ lib/learning.ts
@@ -12,10 +12,6 @@ const SKILLS_LEARNING: Skill[] = [
 		icon: "https://img.shields.io/badge/redis-%23DD0031.svg?style=for-the-badge&color=black&logo=redis",
 	},
 	{
-		name: "AWS",
-		icon: "https://img.shields.io/badge/AWS-%23FF9900.svg?style=for-the-badge&color=black&logo=amazon-aws",
-	},
-	{
 		name: "Cloudflare",
 		icon: "https://img.shields.io/badge/Cloudflare-F38020?style=for-the-badge&color=black&logo=Cloudflare",
 	},
